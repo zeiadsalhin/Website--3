fff42fca22bee15516a520d2ca11c3b859a79508
zeiadsalhin <zeyadali011@gmail.com>
2023-04-03 19:51:25 +0200

    v5

--- Main.js
+++ Main.js
@@ -22,3 +22,25 @@ function changeimage() {
 }
 
 window.onload = changeimage;
+
+//
+
+// reveal items
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var revealitems = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < revealitems.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = revealitems[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      revealitems[i].classList.add("active");
+    } else {
+      revealitems[i].classList.remove("active");
+    }
+  }
+}
